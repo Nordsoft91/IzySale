5bcc38b9971742f2be1d0dda3c8effff654e14da
nordsoft <nordsoft@rambler.ru>
2021-11-23 01:37:13 +0300

    Barcode input field works

--- frontend/src/SalePage.js
+++ frontend/src/SalePage.js
@@ -1,9 +1,31 @@
 import React, { Component } from 'react';
 import { Button, Row, Col, Card, ListGroup, ListGroupItem, Table, Container, Nav, Form } from 'react-bootstrap';
+import { ThemeConsumer } from 'react-bootstrap/esm/ThemeProvider';
 import ServiceAPI from './ServiceAPI';
 import { API_URL } from './ServiceAPI';
 
 const service = new ServiceAPI();
+
+function BarcodeForm({ onSubmitBarcode }) {
+    function handleSubmit(event) {
+        event.preventDefault()
+        onSubmitBarcode(event.currentTarget.elements.barcodeInput.value)
+        event.currentTarget.elements.barcodeInput.value = ''
+    }
+
+    return (
+        <Form onSubmit={handleSubmit}>
+            <Row>
+                <Col>
+                    <Form.Control id="barcodeInput" placeholder="Barcode" />
+                </Col>
+                <Col>
+                    <Button type="submit">Submit</Button>
+                </Col>
+            </Row>
+        </Form>
+    )
+}
 
 class SalePage extends Component {
 
@@ -14,7 +36,11 @@ class SalePage extends Component {
             nextPageURL: '',
             cartItems: []
         };
+        this.componentDidMount = this.componentDidMount.bind(this);
         this.nextPage = this.nextPage.bind(this);
+        this.handleSell = this.handleSell.bind(this);
+        this.handleDelete = this.handleDelete.bind(this);
+        this.handleBarcode = this.handleBarcode.bind(this);
     }
 
 
@@ -56,9 +82,18 @@ class SalePage extends Component {
         });
     }
 
-    handleBarcode(e) {
+    handleBarcode(code) {
         var self = this;
-        
+        service.searchByBarcode(code)
+        .then(function(item) {
+            return service.addToCart(item.data.pk);
+        })
+        .then(function() {
+            return service.getCartList();
+        })
+        .then(function(result) {
+            self.setState({ cartItems: result.data });
+        });
     }
 
     render() {
@@ -137,16 +172,7 @@ class SalePage extends Component {
                                     </tbody>
                                 </Table>
                             </Nav.Item>
-                            <Form>
-                                <Row>
-                                    <Col>
-                                        <Form.Control placeholder="Barcode" />
-                                    </Col>
-                                    <Col>
-                                        <Button variant="primary" onClick={(e) => this.handleBarcode(e)} type="submit">Submit</Button>
-                                    </Col>
-                                </Row>
-                            </Form>
+                            <BarcodeForm onSubmitBarcode={this.handleBarcode}/>
                         </Nav>
                     </Col>
 
