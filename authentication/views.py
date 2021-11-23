9d7f02e49624a0067ee52455aee72f2a5c233c14
nordsoft <nordsoft@rambler.ru>
2021-11-23 23:46:31 +0300

    Make auth view

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import permissions, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .serializers import CustomTokenObtainPairSerializer, CustomUserSerializer
 
 # Create your views here.
+class ObtainTokenPairWithColorView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
+
+class CustomUserCreate(APIView):
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request, format='json'):
+        serializer = CustomUserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            if user:
+                json = serializer.data
+                return Response(json, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
